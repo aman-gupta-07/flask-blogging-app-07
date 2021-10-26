c628ca6d3029d83f83a20dc7f765987dc1f77771
Aman Gupta <imamangupta2019@gmail.com>
2021-10-27 02:03:26 +0530

    Initialize Git repository

---
+++ app.py
@@ -0,0 +1,52 @@
+
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba007'
+
+posts = [
+    {
+        'author': 'Aman Gupta',
+        'title': 'Blog Post 1',
+        'content': 'The data revolution has begun to analysis and visualize the data there will be whole lot of requirement of data analysts and developers.',
+        'date_posted': 'October 09, 2021'
+    },
+
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
